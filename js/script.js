857c3bf45b3fd4faa6ef49270a6c03277f455776
Lucas Falcão <lucasmelohue@gmail.com>
2022-08-08 13:34:54 -0300

    v1.1
    design fixes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function calcular() {
-    let num1 = parseFloat(prompt('Digite a primeira nota'));
-    let num2 = parseFloat(prompt('Digite a segunda nota'));
-    let num3 = parseFloat(prompt('Digite a terceira nota'));
+    let num = parseFloat(prompt('Digite a primeira nota'));
+    num += parseFloat(prompt('Digite a segunda nota'));
+    num += parseFloat(prompt('Digite a terceira nota'));
     let min = parseFloat(prompt('Digite a nota minima para aprovação'));
-    let med = (num1+num2+num3)/3;
+    let med = num/3;
     let aprv = med >= min ? 'aprovado' : 'reprovado';
     let res = document.querySelector('section#res');
 
